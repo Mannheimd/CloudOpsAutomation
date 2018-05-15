59d5339be93a83113e767704364bfb368039be99
Chris Manders <cmanders159@gmail.com>
2018-05-15 13:01:39 +0100

    Add pre-built Dropbox interation code

---
+++ Dropbox/DropboxDataCleanup/DropboxDataCleanup/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Dropbox.Api;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DropboxDataCleanup
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+    }
+
+    public static class ApplicationVariables
+    {
+        private static DropboxClient _dropboxClient;
+        public static DropboxClient dropboxClient
+        {
+            get
+            {
+                if (_dropboxClient == null)
+                {
+                    _dropboxClient = DropboxAuth.SetupClient();
+                }
+                return _dropboxClient;
+            }
+        }
+    }
+}
