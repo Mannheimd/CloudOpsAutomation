76c4db906b3c9e68570199aef2d448f1b9de322e
Chris Manders <cmanders159@gmail.com>
2018-05-16 12:56:04 +0100

    In progress building methods for checking folder age

--- Dropbox/DropboxDataCleanup/DropboxDataCleanup/App.xaml.cs
+++ Dropbox/DropboxDataCleanup/DropboxDataCleanup/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Dropbox.Api;
+using Dropbox.Api.Files;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
@@ -14,7 +15,58 @@ namespace DropboxDataCleanup
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            Startup += AppStartup;
+        }
 
+        void AppStartup(object sender, StartupEventArgs e)
+        {
+            MessageBox.Show("Hello");
+            Current.Shutdown();
+        }
+    }
+
+    public static class DropboxTasks
+    {
+        public static async List<Metadata> GetOutOfDateItemsAsync(string path, TimeSpan maxAge)
+        {
+            DropboxClient client = ApplicationVariables.dropboxClient;
+            List<Metadata> returnList = new List<Metadata>();
+
+            ListFolderResult content;
+            try
+            {
+                content = await client.Files.ListFolderAsync(path, recursive: true);
+            }
+            catch
+            {
+                //TODO: Add error handling
+                return null;
+            }
+
+            if (content == null)
+                return null;
+
+            foreach (Metadata item in content.Entries)
+            {
+                if (item.IsFile
+                    && item.AsFile.ServerModified < DateTime.Now - maxAge)
+                {
+                    returnList.Add(item);
+                }
+                else if (item.IsFolder
+                    && !CheckForInDateContent(item.AsFolder, maxAge))
+                {
+
+                }
+            }
+        }
+
+        private static bool CheckForInDateContent(FolderMetadata folder, TimeSpan maxAge)
+        {
+            
+        }
     }
 
     public static class ApplicationVariables
