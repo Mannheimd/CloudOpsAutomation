3933804a8b2b4b50a2c66cc2c3f079f3d85cb1b0
Chris Manders <cmanders159@gmail.com>
2018-05-23 21:07:09 +0100

    Remove unnecessary usings

--- Dropbox/DropboxDataCleanup/DropboxDataCleanup/App.xaml.cs
+++ Dropbox/DropboxDataCleanup/DropboxDataCleanup/App.xaml.cs
@@ -2,10 +2,6 @@
 using Dropbox.Api.Files;
 using System;
 using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.IO;
-using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 using Log_Handler;
